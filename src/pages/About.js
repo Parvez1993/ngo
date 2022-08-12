38441f22fecddb136845d75d06086e670ce15749
Parvez1993 <mohammedparvez1923@gmail.com>
2022-08-12 12:58:05 +0600

    fixes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import AboutCared from "../components/About/AboutCared";
 import Banner from "../components/Homepage/Banner";
-import user1 from "../data/user1.jpg";
 import Typed from "typed.js";
 function About() {
   // Create Ref element.
@@ -10,15 +8,15 @@ function About() {
   useEffect(() => {
     const typed = new Typed(el.current, {
       strings: [
-        `Samsara Foundation was created to help girls in India get the
-      resources they need to educate themselves and improve their lives.`,
+        `I am passionate about teaching and finding different ways to communicate with my students from using technology to teach underprivileged teens in Burma. Establishing this non-profit organization was a step in the direction to further solidify my commitment to my passion to kids in need.`,
       ], // Strings to display
       // Speed settings, try diffrent values untill you get good results
-      startDelay: 40,
-      typeSpeed: 80,
+      startDelay: 20,
+      typeSpeed: 30,
+
       backSpeed: 10,
       fadeOutClass: "typed-fade-out",
-      loop: true,
+      loop: false,
       showCursor: true,
     });
 
@@ -27,34 +25,15 @@ function About() {
       typed.destroy();
     };
   }, []);
-  let data = [
-    {
-      id: 1,
-      title: "Radha Gupta",
-      desc: "My name is Radha Gupta. I am 16 years old and from Long Island, NY. I am interested in public health and public policy. I created this organization to help girls receive an education. A girl’s education has been proven to better families, communities, and even countries. In 2018, India was ranked the least safe place to be a women due to a lack of opportunities and violence. I believe that prioritizing a girls’ education can help improve the quality of her life and her family.",
-    },
-    {
-      id: 2,
-      title: "Siya Gupta",
-      desc: "My name is Siya Gupta. As a teenager receiving an education, I get to pursue my hobbies, which include playing tennis and sketching, and also learn about things that interest me. As a result I’m fortunate enough to receive opportunities that girls around the world may not. Education gives everyone the power and tools to improve their lives. I’m grateful for my education opportunities and want to make sure that other girls can also get the same ones.",
-    },
-  ];
 
   return (
     <>
       <Banner show={false} title="About" />
-      <Container className="py-5 my-3">
-        <Row>
-          <Col className="aboutText my-3" lg={4}>
+      <Container className="py-5 my-4">
+        <Row style={{ minHeight: "50vh" }}>
+          <Col className="aboutText my-3" lg={12}>
             <span ref={el}></span>
           </Col>
-          {data.map((i, k) => {
-            return (
-              <Col lg={4} key={k} className="py-3">
-                <AboutCared title={i.title} desc={i.desc} image={user1} />
-              </Col>
-            );
-          })}
         </Row>
       </Container>
     </>
